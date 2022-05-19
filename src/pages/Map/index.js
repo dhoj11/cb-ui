53cd358f089f9d631ef7c1a7bbefcec6994980fa
정동호 <nearermg@gmail.com>
2022-05-19 19:59:26 +0900

    [지도] 로드속도 저하에 따른 마커 클러스터 미사용

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -73,12 +73,12 @@ const Map = observer(() => {
 
     useEffect(()=>{
 
-        setClusterer(
-            new kakao.maps.MarkerClusterer({
-                map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
-                averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-                minLevel: 4 // 클러스터 할 최소 지도 레벨
-        }));
+        // setClusterer(
+        //     new kakao.maps.MarkerClusterer({
+        //         map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
+        //         averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+        //         minLevel: 4 // 클러스터 할 최소 지도 레벨
+        // }));
 
     },[map])
 
@@ -191,9 +191,9 @@ const Map = observer(() => {
             });
         }
 
-        if(!validation.checkEmpty(clusterer) && coords.length > 0) {
-            clusterer.addMarkers(coords);
-        }
+        // if(!validation.checkEmpty(clusterer) && coords.length > 0) {
+        //     clusterer.addMarkers(coords);
+        // }
     }
 
     const onOpenAddDrawer = () => {
