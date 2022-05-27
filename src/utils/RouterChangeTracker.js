3c32c6ca05bc39a3bc53b42f51a0153e810a7b3f
정동호 <nearermg@gmail.com>
2022-05-28 02:33:30 +0900

    [공통] 구글-GA 추가

--- src/utils/RouterChangeTracker.js
+++ src/utils/RouterChangeTracker.js
@@ -3,21 +3,21 @@ import { useLocation } from "react-router-dom";
 import ReactGA from "react-ga";
 
 const RouteChangeTracker = () => {
-    const location = useLocation();
+    //const location = useLocation();
     const [initialized, setInitialized] = useState(false);
 
     useEffect(() => {
-        if (!window.location.href.includes("localhost")) {
+       if (!window.location.href.includes("localhost")) {
             ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID);
         }
         setInitialized(true);
     }, []);
 
-    useEffect(() => {
-        if (initialized) {
-            ReactGA.pageview(location.pathname + location.search);
-        }
-    }, [initialized, location]);
+    // useEffect(() => {
+    //     if (initialized) {
+    //         ReactGA.pageview(location.pathname + location.search);
+    //     }
+    // }, [initialized, location]);
 }
 
 export default RouteChangeTracker;
