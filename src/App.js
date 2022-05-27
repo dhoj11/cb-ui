24ad6721120173332e8525c79821a0d9d343bcb8
정동호 <nearermg@gmail.com>
2022-05-28 01:50:58 +0900

    [공통] 구글-GA 추가

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import {useEffect, useState} from "react";
 import axios from "./utils/axios";
 import {getAccessToken} from "./apis/AuthenticationApis";
 import {Box, CircularProgress} from "@mui/material";
+import RouteChangeTracker from "./utils/RouterChangeTracker"
 
 function App() {
 
-    const [isLoad, setIsLoad] = useState(false)
+    const [isLoad, setIsLoad] = useState(false);
+
+    RouteChangeTracker();
 
     useEffect(()=>{
         fnSetAuthenticationHeader();
@@ -26,34 +29,34 @@ function App() {
         setIsLoad(true);
     }
 
-  return (
-      <>
-        <GlobalStyles/>
-          <BrowserRouter>
-              {
-                  isLoad
-                    ? <SinglePage>
-                         <Routes>
-                              <Route exact path="/" element={<Map/>}/>
-                              <Route path="/map" element={<Map/>}/>
-                         </Routes>
-                     </SinglePage>
-                  : <Box sx={{
-                          width: "100%",
-                          height: "100%",
-                          display: "flex",
-                          justifyContent: "center",
-                          margin: "0 auto",
-                          position: "absolute",
-                          top: "50%"
-                      }}>
-                          <CircularProgress
-                            size={50}/>
-                      </Box>
-              }
-          </BrowserRouter>
-      </>
-  );
+    return (
+        <>
+            <GlobalStyles/>
+            <BrowserRouter>
+                {
+                    isLoad
+                        ? <SinglePage>
+                            <Routes>
+                                <Route exact path="/" element={<Map/>}/>
+                                <Route path="/map" element={<Map/>}/>
+                            </Routes>
+                        </SinglePage>
+                        : <Box sx={{
+                            width: "100%",
+                            height: "100%",
+                            display: "flex",
+                            justifyContent: "center",
+                            margin: "0 auto",
+                            position: "absolute",
+                            top: "50%"
+                        }}>
+                            <CircularProgress
+                                size={50}/>
+                        </Box>
+                }
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
